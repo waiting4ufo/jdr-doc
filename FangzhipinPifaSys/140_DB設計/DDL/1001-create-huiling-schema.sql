11736f179b831b0e7d97034f45c66132337e6992
JDR - Journey to DReam <jdr.ikic@qq.com>
2024-02-14 19:50:46 +0900

    Add files via upload

---
+++ FangzhipinPifaSys/140_DB設計/DDL/1001-create-huiling-schema.sql
@@ -0,0 +1,16 @@
+
+-- 本システムで使うDB（schema)作成
+-- * schema name: huiling
+
+-- mysql -u -root -p
+-- <Enter password:>xxxxxxx
+
+-- ■１
+CREATE SCHEMA `huiling` DEFAULT CHARACTER SET utf8 ;
+
+-- ■２
+-- create user use workbench
+-- jdradmin     DBA (as root)
+
+-- huilinguser  (for schema [huiling] only)
+
